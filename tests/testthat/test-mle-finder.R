f12a5a9418603d60a53a6a41ba80de4b97ba4cd6
Mingyuan Li <mli169@jh.edu>
2023-03-05 07:23:04 -0500

    Restore test tolerance of coefficients to 1e-2

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -11,7 +11,7 @@ test_that("linalg and optim least-sq coincide", {
   )
   expect_true(are_all_close(
     coef(via_linalg_out), coef(via_bfgs_out),
-    abs_tol = 1e-4, rel_tol = 1e-4
+    abs_tol = 1e-2, rel_tol = 1e-2
   ))
 })
 
@@ -28,6 +28,51 @@ test_that("newton and bfgs outputs coincide on logit model", {
   )
   expect_true(are_all_close(
     coef(via_newton_out), coef(via_bfgs_out),
-    abs_tol = 1e-4, rel_tol = 1e-4
+    abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
+
+test_that("newton and bfgs outputs coincide on binomial model", {
+  n_obs <- 32
+  n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = "logit", seed = 1918,
+                        option = list(n_trial=1:n_obs))
+  design <- data$design
+  outcome <- data$outcome
+  via_newton_out <- hiper_glm(design, outcome, model = "logit")
+  via_bfgs_out <- hiper_glm(
+    design, outcome,
+    model = "logit", option = list(mle_solver = "BFGS")
+  )
+  expect_true(are_all_close(
+    coef(via_newton_out), coef(via_bfgs_out),
+    abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
+
+test_that("Bernoulli and binomial with n_trial=1 have matching output",{
+  n_obs <- 32
+  n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = "logit", seed = 1918,
+                        option = list(n_trial=rep(1,n_obs)))
+  design <- data$design
+  outcome <- data$outcome
+  via_newton_out_binary <- hiper_glm(design, outcome, model = "logit")
+  via_bfgs_out_binary <- hiper_glm(
+    design, outcome,
+    model = "logit", option = list(mle_solver = "BFGS")
+  )
+  via_newton_out_binom <- hiper_glm(design, outcome$n_success, model = "logit")
+  via_bfgs_out_binom <- hiper_glm(
+    design, outcome$n_success,
+    model = "logit", option = list(mle_solver = "BFGS")
+  )
+  expect_true(are_all_close(
+    coef(via_newton_out_binom), coef(via_newton_out_binary),
+    abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+  expect_true(are_all_close(
+    coef(via_bfgs_out_binom), coef(via_bfgs_out_binary),
+    abs_tol = 1e-2, rel_tol = 1e-2
   ))
 })
