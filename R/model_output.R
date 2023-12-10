817dc0e82a3ee4fe5d4aaf86b9e897c2f4fc262f
Mingyuan Li <mli169@jhmi.edu>
2023-12-10 17:01:31 -0500

    Modify print generic method for hglm

--- R/model_output.R
+++ R/model_output.R
@@ -2,13 +2,16 @@
 print.hglm <- function(hglm_out) {
   cat("Output of hiper_glm:\n")
   # TODO: Display model information
+  cat(paste("Solver:", hglm_out$solver))
+  cat("Model Coefficients:\n")
+  cat(hglm_out$coefficients)
   warning("print.hglm not yet implemented.")
 }
 #' @export
 coef.hglm <- function(hglm_out) {
   hglm_out$coefficients
 }
-#' @export
+
 vcov.hglm <- function(hglm_out) {
   # TODO: Return covariance matrix of MLE
   warning("vcov.hglm not yet implemented.")
